9b0a0b3a8ed5a3d2b9143826978a0c82ff617e5c
Arpit <gupta.arpit985@gmail.com>
2022-05-29 22:20:12 -0700

    adding 2c scripts

---
+++ script_2c.sh
@@ -0,0 +1,32 @@
+./run_slurm_job.sh --N 1 --px 1 --py 16 --t 10 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 2 --py 8 --t 10 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 4 --py 4 --t 10 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 8 --py 2 --t 10 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 16 --py 1 --t 10 --results_folder results_2c
+
+
+./run_slurm_job.sh --N 1 --px 1 --py 32 --t 20 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 2 --py 16 --t 20 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 4 --py 8 --t 20 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 8 --py 4 --t 20 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 16 --py 2 --t 20 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 32 --py 1 --t 20 --results_folder results_2c
+
+
+./run_slurm_job.sh --N 1 --px 1 --py 64 --t 20 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 2 --py 32 --t 20 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 4 --py 16 --t 20 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 8 --py 8 --t 20 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 16 --py 4 --t 20 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 32 --py 2 --t 20 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 64 --py 1 --t 20 --results_folder results_2c
+
+
+./run_slurm_job.sh --N 1 --px 1 --py 128 --t 30 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 2 --py 64 --t 30 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 4 --py 32 --t 30 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 8 --py 16 --t 30 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 16 --py 8 --t 30 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 32 --py 4 --t 30 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 64 --py 2 --t 30 --results_folder results_2c
+./run_slurm_job.sh --N 1 --px 128 --py 1 --t 30 --results_folder results_2c
